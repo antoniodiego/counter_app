4ab42fc762cb2873ed9c1c74e9ea5587d43e4a72
Antônio Diego Silva <antoniodiegoluz@hotmail.com>
2021-07-03 22:29:32 -0300

    Copiei o código fonte de outra pasta para a pasta versionada

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 5;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Meu primerio app"),
+        ),
+        body: Center(
+            child: Text(
+          "Contador\n$count",
+          textAlign: TextAlign.center,
+        )),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              increment();
+            }));
+  }
+}
